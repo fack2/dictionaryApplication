99ebf0eeb2cedeb13e49b42911eadfeea983d1f3
Rand Inaim <rand.inaim95@gmail.com>
2019-07-03 09:10:13 +0300

    rand before selector

--- js/Dom.js
+++ js/Dom.js
@@ -11,28 +11,30 @@ function request(url, cb) {
 		});
 }
 
-var input = document.getElementById('inpiut');
+var input = document.getElementById('input');
 var inputLang = document.getElementById('inputLang');
 var translateButton = document.getElementById('translate');
-var transaltion = document.getElementById('translation');
-var transaltion = document.getElementById('detectedLang');
-
-var detectUrl =
-	'https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20190702T084028Z.6d9a7275087030e7.c7f5e34f73df150db59b46361478dc0c93bfd86c';
+var translation = document.getElementById('translation');
+var detectedLang = document.getElementById('detectedLang');
 
 translateButton.addEventListener('click', function() {
 	var text = input.value;
-	detectUrl += '&text=' + input.value; //get the input text and add it to the api
+	console.log('input value', text);
+	var detectUrl = `https://translate.yandex.net/api/v1.5/tr.json/detect?key=trnsl.1.1.20190702T084028Z.6d9a7275087030e7.c7f5e34f73df150db59b46361478dc0c93bfd86c&text=${text}`;
 
-	request(url, (data) => {
-		console.log(data.lang);
-		detectedLang.innerText = data.lang; //get the detected lang and add it to the api
+	//get the input text and add it to the api
+	console.log(detectUrl);
+
+	detectedLang.value = '';
+	translation.value = '';
+	request(detectUrl, (data) => {
+		console.log(data);
+		detectedLang.value = data.lang; //get the detected lang and add it to the api
 
 		translateUrl = `https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20190702T084028Z.6d9a7275087030e7.c7f5e34f73df150db59b46361478dc0c93bfd86c&text=${text}&lang=${data.lang}-${inputLang.value}`;
-		translateUrl += '&text=' + input.value;
-		translateUrl += '&lang=' + data.lang + '-' + inputLang.value;
 		request(translateUrl, (data) => {
-			transaltion.innerText = data.text[0];
+			console.log(data.text[0]);
+			translation.value = data.text[0];
 		});
 	});
 });
